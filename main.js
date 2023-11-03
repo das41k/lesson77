3d595ad4ec00b5d1929690c829fd24d8b34fce43
das41k <148576114+das41k@users.noreply.github.com>
2023-11-03 22:19:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const a = 10
+const b = 8
+const c = "Hello"
+const d = "World"
+const summ = a + b
+const razn = a - b
+const string = c + d
+const proiz = a*b
+const mod = a % b
+const del = a / b
+const step = a**b
+console.log(summ,razn,string,proiz,mod,del,step)
